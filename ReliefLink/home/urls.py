85739569a54ce5df390163d6d717d9fc9aeeb39e
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2024-12-12 21:08:17 +0600

    home link fixed

--- ReliefLink/home/urls.py
+++ ReliefLink/home/urls.py
@@ -1,9 +1,8 @@
-# home/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='home'),  
     path('service/', views.service, name='service'),
     path('contact/', views.contact, name='contact'),
     path('login/', views.login_view, name='login'),
