e9ca07ecd38e21aa39919bca11e2767a960fab22
Azazul <islamazazul72@gmail.com>
2025-01-04 00:09:02 +0600

    implement role base dashboard, set permission for the specific role to add specific user

--- ReliefLink/account/urls.py
+++ ReliefLink/account/urls.py
@@ -1,3 +1,5 @@
+# account/urls.py
+
 from django.urls import path
 from . import views
 
@@ -5,5 +7,22 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
+
     path('password_update/', views.updatepassword_view, name='update_password'),
+
+    path('add_user/', views.add_user_view, name='add_user'),
+
+    path('add_divisionalcommissioner/', views.add_divisionalcommissioner_view, name='add_divisionalcommissioner'),
+
+    path('add_deputycommissioner/', views.add_deputycommissioner_view, name='add_deputycommissioner'),
+
+    path('add_uno/', views.add_uno_view, name='add_uno'),
+
+    path('add_unionchairman/', views.add_unionchairman_view, name='add_unionchairman'),
+
+    path('add_wardmember/', views.add_wardmember_view, name='add_wardmember'),
+
+    path('add_house/', views.add_house_view, name='add_house'),
+
+
 ]
