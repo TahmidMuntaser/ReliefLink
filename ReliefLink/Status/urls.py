415860bfa8f2731d38d6aabe9a5761329a5bf8f5
Azazul <islamazazul72@gmail.com>
2025-01-30 13:29:23 +0600

    add status for division and district

--- ReliefLink/Status/urls.py
+++ ReliefLink/Status/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.status, name = "status"),
+    path('', views.status, name="status"),
+    path('District Status/<int:division_id>/', views.district_status, name="district_status"),
 ]
--- ReliefLink/Status/views.py
+++ ReliefLink/Status/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+# Status/views.py
+from django.shortcuts import render, get_object_or_404
+from home.models import Division, District
+import json
 
-# Create your views here.
 def status(request):
-    return render(request, 'home/status.html')
+    floody_divisions = Division.objects.exclude(floody_districts='[]')
+    return render(request, 'status/status.html', {'floody_divisions': floody_divisions})
+
+
+def district_status(request, division_id):
+    division = get_object_or_404(Division, id=division_id)
+    
+    districts = District.objects.filter(division=division)
+
+    filtered_districts = []
+    for district in districts:
+        try:
+            floody_unions = json.loads(district.floody_upazilas)
+            if floody_unions:
+                filtered_districts.append(district)
+        except json.JSONDecodeError:
+            continue
+    
+    return render(request, 'status/district_status.html', {
+        'division' : division,
+        'districts': filtered_districts,
+    })
