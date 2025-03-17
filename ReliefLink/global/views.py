93749776b66af5e2242ef4c3a0295667462d34b4
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2025-03-17 15:56:21 +0600

    login is req to enter status page

--- ReliefLink/global/views.py
+++ ReliefLink/global/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+@login_required(login_url='login')
+def status_view(request):
+    return render(request, 'status/status.html')
 
 
-# Create your views here.
