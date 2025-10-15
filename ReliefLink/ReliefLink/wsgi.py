b60fe9ed7783fc35e79632ef18e5d6d027f4ab28
Tahmid <tahmid25muntaser@gmail.com>
2025-10-16 01:10:25 +0600

    Fix WSGI for Vercel - add app variable

--- ReliefLink/ReliefLink/wsgi.py
+++ ReliefLink/ReliefLink/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ReliefLink.settings')
 
 application = get_wsgi_application()
+
+# Vercel expects 'app' variable
+app = application
