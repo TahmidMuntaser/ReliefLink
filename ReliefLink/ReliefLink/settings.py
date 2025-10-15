109bf1eb4ebb27e2339ecce33a7acafc91d5c619
Tahmid <tahmid25muntaser@gmail.com>
2025-10-16 01:26:12 +0600

    Fix static files for Vercel deployment

--- ReliefLink/ReliefLink/settings.py
+++ ReliefLink/ReliefLink/settings.py
@@ -153,11 +153,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Static file finders
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 # Use compressed, hashed static file storage for WhiteNoise in production
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Security/Proxy settings for Vercel
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
